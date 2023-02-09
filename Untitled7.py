09913b4b0839aafa6de98ef006d0f2878db6816f
Sahilydv222 <109260381+Sahilydv222@users.noreply.github.com>
2023-02-09 23:28:42 +0530

    Add files via upload

---
+++ Untitled7.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# Sahil kumar
+
+# Engineering Analyst
+
+# I am interested in data science because, I want to combine the power of AI/ML with the business requirements, to create powerful solutions and provide deep insights into issues an organization needs to solve or discover underlying patterns to discover new potential insights
+
+# The Following Code will generate a random integer between 80 and 100
+
+# In[2]:
+
+
+import random
+
+Random_int = random.randint(80,100)
+
+print(Random_int)
+
+
+# Interested technologies:
+1 AI- ML/DL
+2 Cloud
+3 Data Analytics
+4 IoT
+# In[ ]:
+
+
+
+
